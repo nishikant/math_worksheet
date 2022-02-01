6346d4e5bfce6142a9c0ef2740bd991d6efc988f
Gattu <gattusevalkar@gmail.com>
2022-02-01 23:09:07 +0530

    math worksheet creator

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # math_worksheet
-Generate math worksheets
+
+Generate math worksheets in PDF format.
+
+This script generates a PDF worksheet for kids to practice addition,
+subtraction, multiplication and division. It generates two random
+numbers and use those to create math problems for addition,
+subtraction, multiplication and division. You can change the range of
+random numbers using --start and --end parameters. By default it will
+generate random number between 0 and 100. 2 pages are generated with
+20 problems on each page. You can increase the number of pages using
+--pages option.
+
+This program will generate a "questions.pdf" file in current working
+directory.
+
+```
+usage: math_questions.py [-h] [--pages PAGES] [--start START] [--end END]
+
+optional arguments:
+  -h, --help     show this help message and exit
+  --pages PAGES  number of pages, default:2
+  --start START  Random starting nos. default:0
+  --end END      Randmon ending nos. default:100
+  
+eg: $>./math_questions.py --end 1000
+$> ./math_questions.py --start 10 --end 399 --pages 6
+```
+
---
+++ math_questions.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+import argparse
+import random
+import logging
+from fpdf import FPDF
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def create_pdf(pages, start, end):
+    """
+    Math Worksheet creator.
+
+    Creates a MATH PDF worksheets for addition,
+    subtraction, multiplication and division
+
+    """
+    pdf = FPDF()
+    pdf.add_page()
+
+    pdf.set_font("Arial", size=14)
+
+    loop_count = pages * 5 + 1
+
+    for i in range(1, loop_count):
+        num1 = random.randint(start, end)
+        num2 = random.randint(start, end)
+
+        padding = len(str(num1)) if num1 > num2 else len(str(num2))
+
+        line1_space = 27
+        line2_space = 23
+        line3_space = line2_space + padding - 3
+        dash_count = 3 + padding + 3
+
+        padding = '0'+str(padding)
+
+        divident = num1 if num1 > num2 else num2
+        divisor = num2 if num1 > num2 else num1
+
+        add = " "*4 + "{:{}}".format(num1, padding) + \
+            " "*line1_space + "{:{}}".format(num1, padding) + \
+            " "*line1_space + "{:{}}".format(num1, padding) + \
+            " "*line1_space + "{:{}}".format(divident, padding)
+
+        pdf.cell(95, 8, txt=add,  ln=1, align='L')
+
+        sub = "+  {:{}}".format(num2, padding) + " "*line2_space + \
+            "-   {:{}}".format(num2, padding) + " "*line2_space + \
+            "x  {:{}}".format(num2, padding) + " "*line2_space + \
+            "{}  {:{}}".format(chr(247), divisor, padding)
+
+        pdf.cell(95, 8, txt=sub,  ln=1, align='L')
+
+        dash = "-" * dash_count + " "*line3_space + "-"*dash_count + " " * \
+            line3_space + "-"*dash_count + " "*line3_space + "-"*dash_count
+
+        pdf.cell(95, 8, txt=dash,  ln=1, align='L')
+
+        if (i % 5 != 0):
+            pdf.cell(95, 30, txt=" ",  ln=1, align='L')
+        elif i != loop_count - 1:
+            pdf.add_page()
+
+    pdf.output("questions.pdf")
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pages",
+                        default=2,
+                        type=int,
+                        help="number of pages, default:2",
+                        action="store")
+    parser.add_argument("--start",
+                        default=0,
+                        dest="start",
+                        type=int,
+                        help="Random starting nos. default:0",
+                        action="store")
+    parser.add_argument("--end",
+                        default=100,
+                        dest="end",
+                        type=int,
+                        help="Randmon ending nos. default:100",
+                        action="store")
+    args = parser.parse_args()
+    create_pdf(args.pages, args.start, args.end)
